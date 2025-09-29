4d9532846621f6d1d17743cf6d63999fae2e840b
tinagu319 <3563805137@qq.com>
2025-09-29 00:31:12 -0400

    added Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -2,11 +2,15 @@ public class Driver {
 public static void main(String [] args) {
 Polynomial p = new Polynomial();
 System.out.println(p.evaluate(3));
-double [] c1 = {6,0,0,5};
-Polynomial p1 = new Polynomial(c1);
-double [] c2 = {0,-2,0,0,-9};
-Polynomial p2 = new Polynomial(c2);
+double [] c1 = {6,5};
+int[] e1 = {0,3};
+Polynomial p1 = new Polynomial(c1,e1);
+double [] c2 = {-2,-9};
+int[] e2 = {1,4};
+Polynomial p2 = new Polynomial(c2,e2);
 Polynomial s = p1.add(p2);
+Polynomial s1=p1.multiply(p2);
+p1.saveToFile();
 System.out.println("s(0.1) = " + s.evaluate(0.1));
 if(s.hasRoot(1))
 System.out.println("1 is a root of s");
